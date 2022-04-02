69e7e2f5eaa7f3185dda9c87856c6d68b500bd82
Urs Utzinger <utzinger@email.arizona.edu>
2022-04-01 17:25:22 -0700

    Renamed Scripts

---
+++ Intake_Cam.bat
@@ -0,0 +1,7 @@
+cd %HOMEDRIVE%%HOMEPATH%
+cd Desktop
+gst-launch-1.0^
+ rtspsrc location=rtsp://10.41.83.12:8554/test buffer-mode=synced protocols=tcp+udp+udp-mcast !^
+ queue max-size-buffers=0 max-size-time=0 max-size-bytes=0 min-threshold-time=0 !^
+ decodebin ! videoconvert !^
+ autovideosink sync=false
